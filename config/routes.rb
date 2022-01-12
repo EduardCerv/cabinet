2c7c9fd2a2b05c77643b708f12acc6b5b697b117
Eduardo Cervantes <eduardo.cervantes@bigcommerce.com>
2022-01-12 17:12:00 -0600

    Doc controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # This make the home page is going to the controller#action that you need for this example in welcome contrller and index action
   root 'welcome#index'
 
+  resources :docs
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
