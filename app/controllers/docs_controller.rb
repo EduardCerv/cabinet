aa4dce81a4916a5ea87fd09e940211b52c129dd8
Eduardo Cervantes <eduardo.cervantes@bigcommerce.com>
2022-01-12 17:44:43 -0600

    Add information for new, create and, index actions.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,15 +1,26 @@
 class DocsController < ApplicationController
+    #excete find_doc function just for the specific actions
+    before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
     def index
+        @docs = Doc.all.order("created_at DESC")
     end
 
     def show
     end
-
+    #shows the page where you can create a Doc
     def new
+        @doc = Doc.new
     end
-
+    #This action create the Doc and return to respective template
     def create
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -24,8 +35,10 @@ class DocsController < ApplicationController
     private
 
         def find_doc
+            @doc = Doc.find(params[:id])
         end
 
         def doc_params
+            params.require(:doc).permit(:title, :body)
         end
 end
