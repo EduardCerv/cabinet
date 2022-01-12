16a4b20f9123ae344804a5a580ba49c168e0e07d
Eduardo Cervantes <eduardo.cervantes@bigcommerce.com>
2022-01-12 15:34:32 -0600

    generate welcome controller and set index like home url in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  # This make the home page is going to the controller#action that you need for this example in welcome contrller and index action
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
