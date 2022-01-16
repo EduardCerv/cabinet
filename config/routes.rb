b0fc80fb57a288fbf14c2dfc4d1a0c19bd899cf3
Eduardo Cervantes <eduardo.cervantes@bigcommerce.com>
2022-01-15 23:33:41 -0600

    Cahnge the root of application when the user is authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
+  resources :docs
+  authenticate :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
   # This make the home page is going to the controller#action that you need for this example in welcome contrller and index action
   root 'welcome#index'
-
-  resources :docs
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
